a1fda67cbc9c3233b3a8498df2daf65838d3ef3d
KingMob <john@onethreethree.net>
2019-10-23 16:50:30 +0100

    turns on and off layers smoothly

--- src/reducers/CommunityAssetsReducer.js
+++ src/reducers/CommunityAssetsReducer.js
@@ -2,12 +2,15 @@ const INITIAL_STATE = {
     activeCommunityAssets: [],
 }
 let activeCommunityAssets;
+let index;
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'ASSET_TYPE_ON':
             console.log("turning on " + action.payload.communityAssetsType);
-            state.activeCommunityAssets.push(action.payload.communityAssetsType);
+            index = state.activeCommunityAssets.indexOf(action.payload.communityAssetsType);
+            if(index < 0)
+                state.activeCommunityAssets.push(action.payload.communityAssetsType);
             console.log(state.activeCommunityAssets);
             activeCommunityAssets = state.activeCommunityAssets.slice();
             return {
@@ -16,23 +19,15 @@ export default (state = INITIAL_STATE, action) => {
             };
         case 'ASSET_TYPE_OFF':
             console.log("turning off " + action.payload.communityAssetsType);
-            state.activeCommunityAssets.length = 0;
+            index = state.activeCommunityAssets.indexOf(action.payload.communityAssetsType);
+            if(index >= 0)
+                state.activeCommunityAssets.splice(index,1);
             console.log(state.activeCommunityAssets);
             activeCommunityAssets = state.activeCommunityAssets.slice();
             return {
                 ...state,
                 activeCommunityAssets: activeCommunityAssets,
             };
-        /*case 'ADD_MARKER':
-            console.log("Adding " + action.payload.name + " marker at " + action.payload.location + " costing: " + action.payload.price);
-            markerInformationSet.push(action.payload);
-            state.markerInformationSet = markerInformationSet;
-            console.log(markerInformationSet);
-            return state;
-        case 'CLEAR MARKERS':
-            markerInformationSet = [];
-            state.markerInformationSet = markerInformationSet;
-            return state;*/
         default:
             return state;
     }
