269fcfc4a5e02942bbdfc4f117c40be09025ba57
John Evans <38507954+King-Mob@users.noreply.github.com>
2022-04-21 17:58:17 +0100

    add popup on marker click to display description

--- src/components/MapDataGroups.js
+++ src/components/MapDataGroups.js
@@ -3,7 +3,57 @@ import axios from "axios/index";
 import constants from "../constants";
 import { getAuthHeader } from "./Auth";
 import { useDispatch, useSelector } from "react-redux";
-import { Marker } from "react-mapbox-gl";
+import { Marker, Popup } from "react-mapbox-gl";
+
+const DataGroupMarker = ({ marker }) => {
+    const [visible, setVisible] = useState(false);
+
+    return <>
+        <Marker
+            key={marker.idmarkers}
+            coordinates={{
+                lng: marker.location.coordinates[0],
+                lat: marker.location.coordinates[1]
+            }}
+            name={marker.name}
+            description={marker.description}
+            anchor="bottom"
+            style={{ height: '40px', zIndex: 1 }}
+            onClick={() => setVisible(true)}
+        >
+            <div className="marker-icon" style={{
+                height: 40,
+                width: 40,
+                zIndex: 1
+            }}></div>
+        </Marker>
+        {visible && <Popup
+            coordinates={{
+                lng: marker.location.coordinates[0],
+                lat: marker.location.coordinates[1]
+            }}
+            offset={{
+                'bottom': [0, -45]
+            }}
+            className="datagroup-popup"
+            style={{
+                zIndex: 2
+            }}
+        >
+            <div>
+                <div
+                    className="popup-close"
+                    onClick={() => {
+                        setVisible(false)
+                    }}
+                />
+                <h3>{marker.name}</h3>
+                <p>{marker.description}</p>
+            </div>
+        </Popup>}
+
+    </>
+}
 
 const MapDataGroups = () => {
     const dispatch = useDispatch();
@@ -29,23 +79,8 @@ const MapDataGroups = () => {
 
     const dataGroupMarkers = activeDataGroups && activeDataGroups.map(dataGroup =>
         dataGroup.markers.map(marker =>
-            <Marker
-                key={marker.idmarkers}
-                coordinates={{
-                    lng: marker.location.coordinates[0],
-                    lat: marker.location.coordinates[1]
-                }}
-                name={marker.name}
-                description={marker.description}
-                anchor="bottom"
-                style={{ height: '40px', zIndex: 1 }}
-            >
-                <div className="marker-icon" style={{
-                    height: 40,
-                    width: 40,
-                    zIndex: 1
-                }}></div>
-            </Marker>)
+            <DataGroupMarker marker={marker} />
+        )
     );
 
     return <>
