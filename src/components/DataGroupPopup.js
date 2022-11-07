14b5560d103a925febb5e84b4a07ca93af0aec0b
John Evans <38507954+King-Mob@users.noreply.github.com>
2022-11-07 15:50:30 +0000

    add icons, add editing stage, style

--- src/components/DataGroupPopup.js
+++ src/components/DataGroupPopup.js
@@ -7,6 +7,8 @@ import { saveExistingMap } from "../utils/saveMap";
 
 const DataGroupPopup = ({ object, type, visible, closeDescription }) => {
     const [mode, setMode] = useState("display");
+    const [name, setName] = useState(object.name);
+    const [description, setDescription] = useState(object.description);
     const allMaps = useSelector((state) => state.myMaps.maps);
     const maps = allMaps.filter(map => !map.isSnapshot);
     const [selectedMap, setSelectedMap] = useState();
@@ -18,98 +20,135 @@ const DataGroupPopup = ({ object, type, visible, closeDescription }) => {
         body.map = selectedMap;
 
         await saveExistingMap(selectedMap);
-        await axios.post(`${constants.ROOT_URL}/api/user/map/save/${type}`, body, getAuthHeader())
+        await axios.post(`${constants.ROOT_URL}/api/user/map/save/${type}`, body, getAuthHeader());
 
         axios.get(`${constants.ROOT_URL}/api/user/maps/`, getAuthHeader())
             .then((response) => {
                 dispatch({ type: 'POPULATE_MY_MAPS', payload: response.data });
-
-                /*
-                don't load the map you just saved to any more
-
-                const maps = response.data;
-
-                maps.forEach(map => {
-                    if (map.map.eid === selectedMap.map.eid) {
-                        // this bit doesn't work
-                        axios.post(`${constants.ROOT_URL}/api/user/map/view/`, {
-                            "eid": map.map.eid,
-                        }, getAuthHeader())
-                        //pick up the old name for the landDataLayers
-                        console.log(map)
-                        const savedMap = JSON.parse(map.map.data);
-
-                        if (savedMap.mapLayers.activeLayers) {
-                            savedMap.mapLayers.landDataLayers = savedMap.mapLayers.activeLayers;
-                        }
-                        //fix that some have no dataLayers
-                        if (!savedMap.mapLayers.myDataLayers) {
-                            savedMap.mapLayers.myDataLayers = [];
-                        }
-                        dispatch({
-                            type: 'LOAD_MAP',
-                            payload: savedMap,
-                            id: map.map.eid
-                        });
-                    }
-                })*/
             })
+    }
 
+    const editObject = async (newName, newDescription) => {
+        const body = {
+            name: newName,
+            description: newDescription,
+            object
+        };
 
+        await axios.post(`${constants.ROOT_URL}/api/user/edit/${type}`, body, getAuthHeader());
     }
 
     return (
         <div>
-            {visible && (mode === "display") && (
-                <div className="popup-content">
-                    <div className="popup-close" onClick={closeDescription} />
-                    <h3 className="popup-title">{object.name}</h3>
-                    <p className="description-text">{object.description}</p>
-                    <div className="popup-save-icon-container" onClick={() => setMode("save")}>
-                        <div className="popup-save-icon" />
-                    </div>
-                </div>
-            )}
-            {visible && (mode === "save") && (
-                <div className="popup-content">
-                    <div className="popup-close" onClick={() => { setMode("display"); closeDescription(); }} />
-                    <h3 className="popup-title">Save {type} to:</h3>
-                    <div className="popup-save-map-container">
-                        {
-                            maps.map(map =>
-                                <p
-                                    className={`popup-save-map-option ${selectedMap && selectedMap.map.eid === map.map.eid && "save-map-highlighted"}`}
-                                    onClick={() => setSelectedMap(map)}
-                                >
-                                    {map.map.name}
-                                </p>)
-                        }
-                    </div>
-                    <div className="save-map-button"
-                        onClick={() => {
-                            if (selectedMap) {
-                                addObjectToMap(object, selectedMap);
-                                setMode("complete");
-                            }
-                        }}
-                    > Save</div>
-                </div>
-            )}
-            {visible && (mode === "complete") && (
-                <div className="popup-content">
-                    <p className="popup-save-success-text">
-                        {type.slice(0, 1).toUpperCase() + type.slice(1)} successfully saved to
-                        <br />
-                        '{selectedMap.map.name}'
-                    </p>
-                    <div className="save-map-button"
-                        onClick={() => {
-                            closeDescription();
-                            setMode("display");
-                        }}
-                    > Ok</div>
-                </div>
-            )}
+            {visible && <div className="popup-content">
+                <div className="popup-close" onClick={closeDescription} />
+                {(mode === "display") && (
+                    <>
+                        <h3 className="popup-title">{name}</h3>
+                        <div className="popup-body-container">
+                            <div className="popup-body-main">
+                                <p className="description-text">{description}</p>
+                            </div>
+                            <div className="popup-sidebar">
+                                <img src={require("../assets/img/icon-add.svg")}
+                                    onClick={() => { setMode("save") }}
+                                    className="popup-sidebar-button"
+                                />
+                                <img src={require("../assets/img/icon-pencil.svg")}
+                                    className="popup-sidebar-button"
+                                    onClick={() => setMode("edit")}
+                                />
+                            </div>
+                        </div>
+                    </>
+                )}
+                {(mode === "edit") && (
+                    <>
+                        <h3 className="popup-title editable" id="popup-name" contentEditable>{name}</h3>
+                        <div className="popup-body-container">
+                            <div className="popup-body-main">
+                                <p className="description-text editable" id="popup-description" contentEditable>{description}</p>
+                            </div>
+                            <div className="popup-sidebar">
+                                <img src={require("../assets/img/icon-cross.svg")}
+                                    onClick={() => {
+                                        setName(object.name);
+                                        setDescription(object.description);
+                                        setMode("display");
+                                    }}
+                                    className="popup-sidebar-button"
+                                />
+                                <img src={require("../assets/img/icon-tick.svg")}
+                                    className="popup-sidebar-button"
+                                    onClick={() => {
+                                        const newName = document.getElementById("popup-name").textContent;
+                                        const newDescription = document.getElementById("popup-description").textContent;
+                                        setName(newName);
+                                        setDescription(newDescription);
+                                        setMode("display");
+                                        editObject(newName, newDescription);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </>
+                )}
+                {(mode === "save") && (
+                    <>
+                        <h3 className="popup-title">Save {type} to:</h3>
+                        <div className="popup-body-container">
+                            <div className="popup-body-main">
+                                {
+                                    maps.map(map =>
+                                        <p
+                                            className={`popup-save-map-option ${selectedMap && selectedMap.map.eid === map.map.eid && "save-map-highlighted"}`}
+                                            onClick={() => setSelectedMap(map)}
+                                        >
+                                            {map.map.name}
+                                        </p>)
+                                }
+                            </div>
+                            <div className="popup-sidebar">
+                                <img src={require("../assets/img/icon-cross.svg")}
+                                    onClick={() => {
+                                        setMode("display");
+                                    }}
+                                    className="popup-sidebar-button"
+                                />
+                                <img src={require("../assets/img/icon-tick.svg")}
+                                    className="popup-sidebar-button"
+                                    onClick={() => {
+                                        if (selectedMap) {
+                                            addObjectToMap(object, selectedMap);
+                                            setMode("complete");
+                                        }
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </>
+                )}
+                {(mode === "complete") && (
+                    <>
+                        <div className="popup-body-container no-popup-title">
+                            <div className="popup-body-main"><p className="popup-save-success-text">
+                                {type.slice(0, 1).toUpperCase() + type.slice(1)} successfully saved to
+                                <br />
+                                '{selectedMap.map.name}'
+                            </p></div>
+                            <div className="popup-sidebar">
+                                <img src={require("../assets/img/icon-tick.svg")}
+                                    className="popup-sidebar-button"
+                                    onClick={() => {
+                                        closeDescription();
+                                        setMode("display");
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>}
         </div>
     );
 }
