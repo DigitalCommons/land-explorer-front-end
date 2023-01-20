bea3b172df0362ad69e564ec898fadeb7e4c09c3
John Evans <38507954+King-Mob@users.noreply.github.com>
2023-01-20 15:04:26 +0000

    functionalise Modals

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import Save from './modals/Save';
 import MyMaps from './modals/MyMaps';
 import MySharedMaps from './modals/MySharedMaps';
@@ -10,25 +9,16 @@ import EmailShare from './modals/EmailShare';
 import Download from './modals/Download';
 import LinkShare from './modals/LinkShare';
 
-class Modals extends Component {
-    render() {
-        let { drawControl, redrawPolygons } = this.props;
-        return (
-            <div>
-                <EmailShare />
-                <Download />
-                <LinkShare />
-                <Save />
-                <MyMaps drawControl={drawControl} redrawPolygons={redrawPolygons} />
-                <MySharedMaps drawControl={drawControl} redrawPolygons={redrawPolygons} />
-                <OpenMap drawControl={drawControl} redrawPolygons={redrawPolygons} />
-                <Location />
-                <NewMap drawControl={drawControl} />
-            </div>
-        );
-    }
-}
+const Modals = ({ drawControl, redrawPolygons }) => <div>
+    <EmailShare />
+    <Download />
+    <LinkShare />
+    <Save />
+    <MyMaps drawControl={drawControl} redrawPolygons={redrawPolygons} />
+    <MySharedMaps drawControl={drawControl} redrawPolygons={redrawPolygons} />
+    <OpenMap drawControl={drawControl} redrawPolygons={redrawPolygons} />
+    <Location />
+    <NewMap drawControl={drawControl} />
+</div>
 
-Modals.propTypes = {};
-
-export default connect(null)(Modals);
+export default Modals;
