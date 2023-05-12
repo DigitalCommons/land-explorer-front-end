5c2fa6cc17f389777659710de011241a410fd577
rogup <rohit5.gup@gmail.com>
2023-05-12 14:30:34 +0100

    LX-163 Deploy to staging on push to development

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,13 @@
 set -e
 set -x
 
+# If branch was inputted as argument, check it is checked out
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ ! -z "$1" ] && [ $1 != $CURRENT_BRANCH ]; then
+    echo "ERROR: We cannot deploy branch $1 because the branch $CURRENT_BRANCH is checked out"
+    exit 1
+fi
+
 # Pull latest code
 git pull
 
