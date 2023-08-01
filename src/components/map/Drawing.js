4e6b52ba940e5e0cf703264d57d3ac36aae3ff35
ms0ur1s <msouris@gmail.com>
2023-08-01 16:05:34 +0100

    moved duplicate code into function

--- src/components/map/Drawing.js
+++ src/components/map/Drawing.js
@@ -1,99 +1,98 @@
-import React, { useEffect, useState } from 'react';
-import { GeoJSONLayer, Marker } from 'react-mapbox-gl';
+import React, { useEffect, useState } from "react";
+import { GeoJSONLayer, Marker } from "react-mapbox-gl";
 import { useSelector, useDispatch } from "react-redux";
-import DrawingPopup from './DrawingPopup/DrawingPopup';
+import DrawingPopup from "./DrawingPopup/DrawingPopup";
 import * as turf from "@turf/turf";
 
 const Drawing = ({ type, polygon }) => {
-    const [popupClosed, setPopupClosed] = useState(false);
-    const activeTool = useSelector(state => state.leftPane.activeTool);
-    const activePolygon = useSelector(state => state.drawings.activePolygon);
-    const baseLayer = useSelector(state => state.mapBaseLayer.layer);
+  const [popupClosed, setPopupClosed] = useState(false);
+  const activeTool = useSelector((state) => state.leftPane.activeTool);
+  const activePolygon = useSelector((state) => state.drawings.activePolygon);
+  const baseLayer = useSelector((state) => state.mapBaseLayer.layer);
 
-    const isActive = polygon.uuid === activePolygon;
-    const showPopup = !popupClosed && isActive && !activeTool;
+  const isActive = polygon.uuid === activePolygon;
+  const showPopup = !popupClosed && isActive && !activeTool;
 
-    useEffect(() => {
-        if (isActive)
-            setPopupClosed(false);
-    }, [isActive])
+  useEffect(() => {
+    if (isActive) setPopupClosed(false);
+  }, [isActive]);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const drawingLayer = (
-        <GeoJSONLayer
-            key={polygon.uuid}
-            data={polygon.data}
-            linePaint={{
-                "line-color": isActive ? "red" : (baseLayer === 'aerial') ? 'white' : "black",
-                "line-width": (type === "polygon") ? 2 : 3,
-                "line-opacity": activeTool ? 0 : 1,
-            }}
-            fillPaint={(type === "polygon") && {
-                "fill-color": isActive ? "red" : (baseLayer === 'aerial') ? 'white' : "black",
-                "fill-opacity": activeTool ? 0 : .05,
-            }}
-            fillOnClick={(e) => {
-                console.log("Active tool on fill click", activeTool);
-                if (activeTool !== 'drop-pin') {
-                    if (!(activeTool)) {
-                        console.log("the polygon was clicked", e);
-                        if (isActive) {
-                            dispatch({
-                                type: 'CLEAR_ACTIVE_POLYGON',
-                            })
-                        } else {
-                            dispatch({
-                                type: 'SET_ACTIVE_POLYGON',
-                                payload: polygon.uuid
-                            });
-                            setPopupClosed(false);
-                        }
-                    }
-                }
-            }}
-            lineOnClick={(e) => {
-                console.log("Active tool on line click", activeTool);
-                if (activeTool !== 'drop-pin') {
-                    if (isActive) {
-                        dispatch({
-                            type: 'CLEAR_ACTIVE_POLYGON',
-                        })
-                    } else {
-                        dispatch({
-                            type: 'SET_ACTIVE_POLYGON',
-                            payload: polygon.uuid
-                        });
-                        setPopupClosed(false);
-                    }
-                }
-            }}
-        />
-    )
+  const handlePolygonClick = () => {
+    if (activeTool !== "drop-pin") {
+      if (!activeTool) {
+        if (isActive) {
+          dispatch({
+            type: "CLEAR_ACTIVE_POLYGON",
+          });
+        } else {
+          dispatch({
+            type: "SET_ACTIVE_POLYGON",
+            payload: polygon.uuid,
+          });
+          setPopupClosed(false);
+        }
+      }
+    }
+  };
 
-    return <>
-        {drawingLayer}
-        {showPopup && (
-            <Marker
-                key={polygon.uuid + "2"}
-                coordinates={polygon.centre || turf.pointOnFeature(polygon.data).geometry.coordinates}
-                name={polygon.name}
-                description={polygon.description}
-                anchor="bottom"
-                style={{
-                    height: "40px",
-                    zIndex: 4
-                }}
-            >
-                <DrawingPopup
-                    object={polygon}
-                    type={type}
-                    source={"map"}
-                    closeDescription={() => setPopupClosed(true)}
-                />
-            </Marker>
-        )}
-    </>;
-}
+  const drawingLayer = (
+    <GeoJSONLayer
+      key={polygon.uuid}
+      data={polygon.data}
+      linePaint={{
+        "line-color": isActive
+          ? "red"
+          : baseLayer === "aerial"
+          ? "white"
+          : "black",
+        "line-width": type === "polygon" ? 2 : 3,
+        "line-opacity": activeTool ? 0 : 1,
+      }}
+      fillPaint={
+        type === "polygon" && {
+          "fill-color": isActive
+            ? "red"
+            : baseLayer === "aerial"
+            ? "white"
+            : "black",
+          "fill-opacity": activeTool ? 0 : 0.05,
+        }
+      }
+      fillOnClick={handlePolygonClick}
+      lineOnClick={handlePolygonClick}
+    />
+  );
+
+  return (
+    <>
+      {drawingLayer}
+      {showPopup && (
+        <Marker
+          key={polygon.uuid + "2"}
+          coordinates={
+            polygon.centre ||
+            turf.pointOnFeature(polygon.data).geometry.coordinates
+          }
+          name={polygon.name}
+          description={polygon.description}
+          anchor="bottom"
+          style={{
+            height: "40px",
+            zIndex: 4,
+          }}
+        >
+          <DrawingPopup
+            object={polygon}
+            type={type}
+            source={"map"}
+            closeDescription={() => setPopupClosed(true)}
+          />
+        </Marker>
+      )}
+    </>
+  );
+};
 
 export default Drawing;
