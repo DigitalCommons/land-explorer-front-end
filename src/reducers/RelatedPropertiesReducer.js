37ccddad92a2127693e8e61bef0d3f1c6746b28f
rogup <48825436+rogup@users.noreply.github.com>
2023-12-21 15:08:45 +0000

    Lx 284 ownership search error handling (#300)

--- src/reducers/RelatedPropertiesReducer.js
+++ src/reducers/RelatedPropertiesReducer.js
@@ -19,6 +19,7 @@ export default (state = INITIAL_STATE, action) => {
       return {
         ...state,
         properties: {},
+        selectedProperties: {},
         error: action.payload,
         loading: false,
       };
