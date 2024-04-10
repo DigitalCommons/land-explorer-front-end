fc60ad10a5f83ff7828abed59cf7359686433903
ms0ur1s <msouris@gmail.com>
2024-04-10 16:58:50 +0100

    toggle classes

--- src/components/left-pane/LeftPaneToggle.js
+++ src/components/left-pane/LeftPaneToggle.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ToggleSwitch from '../common/ToggleSwitch';
+import React from "react";
+import ToggleSwitch from "../common/ToggleSwitch";
+import useStringToClassName from "../../hooks/useStringToClassName";
 
 const LeftPaneToggle = ({ title, on, onToggle, draggable = false }) => {
-    return (
-        <div
-            className={`tray-item`}
-            onClick={onToggle}
-        >
-            <div className={`tray-item-title ${draggable && 'draggable'}`}>
-                {title}
-            </div>
-            <ToggleSwitch on={on} tooltip="ttShowHideData" />
-        </div>
-    )
-}
+  const dataGroupClassName = useStringToClassName(title);
+
+  return (
+    <div className={`tray-item ${dataGroupClassName ? dataGroupClassName : ''}`} onClick={onToggle}>
+      <div className={`tray-item-title ${draggable && "draggable"}`}>
+        {title}
+      </div>
+      <ToggleSwitch on={on} tooltip="ttShowHideData" />
+    </div>
+  );
+};
 
 export default LeftPaneToggle;
