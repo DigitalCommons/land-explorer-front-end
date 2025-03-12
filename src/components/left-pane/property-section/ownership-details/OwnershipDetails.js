92788e3ce314c4b711547f164958cdbd8ccb3139
ms0ur1s <msouris@gmail.com>
2025-03-12 14:43:12 +0000

    Add show more/less functionality to OwnershipDetails component; enhance button styles and layout

--- src/components/left-pane/property-section/ownership-details/OwnershipDetails.js
+++ src/components/left-pane/property-section/ownership-details/OwnershipDetails.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ProprietorCard from "./proprietor-card/ProprietorCard";
 
 const OwnershipDetails = ({ tenure, inspireId, proprietors, active }) => {
+  const [showMore, setShowMore] = useState(false);
   const proprietorCount = proprietors.length;
+
   return (
     <section>
+      {/* the below `div.ownership-overview` should always visible */}
       <div className="ownership-overview">
         <h3 className="ownership-overview__title">
           <i className="ownership-overview__icon"></i>
@@ -28,16 +31,31 @@ const OwnershipDetails = ({ tenure, inspireId, proprietors, active }) => {
         </div>
       </div>
 
-      {proprietors.map((proprietor, index) => (
-        <ProprietorCard
-          key={index}
-          name={proprietor.name}
-          address={proprietor.address}
-          category={proprietor.category}
-          number={proprietor.number}
-          active={active}
-        />
-      ))}
+      {/* The below should be revealed / hidden when show more / show less is clicked */}
+      {showMore && (
+        <div className="proprietor-list">
+          {proprietors.map((proprietor, index) => (
+            <ProprietorCard
+              key={index}
+              name={proprietor.name}
+              address={proprietor.address}
+              category={proprietor.category}
+              number={proprietor.number}
+              active={active}
+            />
+          ))}
+        </div>
+      )}
+      <div className="property__show-more-container">
+        <button
+          className={`button-text button-show-more ${
+            showMore ? "less" : "more"
+          }`}
+          onClick={() => setShowMore((prev) => !prev)}
+        >
+          {showMore ? "Show Less" : "Show More"}
+        </button>
+      </div>
     </section>
   );
 };
