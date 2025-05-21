ba102a7db9de4985a499998d0d6a14cbec477c6e
ms0ur1s <msouris@gmail.com>
2025-05-21 13:04:13 +0100

    Fix typo in data group color property name: hex_color to hex_colour

--- src/components/left-pane/LeftPaneLandData.js
+++ src/components/left-pane/LeftPaneLandData.js
@@ -169,7 +169,7 @@ const LeftPaneLandData = ({ open, active, onClose }) => {
                 .map((dataGroup) => (
                   <div
                     className={"datagroup-style-wrapper"}
-                    style={{ "--data-group-colour": dataGroup.hex_color }}
+                    style={{ "--data-group-colour": dataGroup.hex_colour, "border": "1px solid #fff" }}
                     key={dataGroup.id}
                   >
                     <LeftPaneToggle
