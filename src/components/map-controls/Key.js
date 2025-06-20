7957a1d8d0b3132393e0a7fbe5460a44523bad19
ms0ur1s <msouris@gmail.com>
2025-06-20 14:36:16 +0100

    update desktop layer key styles

--- src/components/map-controls/Key.js
+++ src/components/map-controls/Key.js
@@ -1,40 +1,53 @@
-import React from 'react';
+import React from "react";
 
-const Key = ({ data, name }) =>
-    <div style={{
-        marginBottom: '24px'
-    }}>
-        <div style={{
-            display: 'inline-block',
-            paddingBottom: '6px',
-            marginTop: 0,
-            marginBottom: 0,
-        }}>{name}</div>
-        <div style={{
-            borderBottom: '1px solid #d0d0d0',
-            width: '100%',
-            marginBottom: '12px',
-        }} />
-        {Object.keys(data).map((key, i) => {
-            return (
-                <div key={i}
-                    style={{
-                        display: 'flex',
-                        marginBottom: '6px'
-                    }}
-                >
-                    <div style={{
-                        backgroundColor: data[key],
-                        opacity: '1',
-                        height: '16px',
-                        width: '16px',
-                        marginRight: '6px'
-                    }} />
-                    <div style={{ fontSize: '14px' }}>{key}</div>
-                </div>
-            )
-        })}
+const Key = ({ data, name }) => (
+  <div
+    style={{
+      marginBottom: "15px",
+    }}
+  >
+    <div
+      style={{
+        display: "inline-block",
+        marginTop: 0,
+        marginBottom: 0,
+        color: "#27ae60",
+        fontSize: "14px",
+        height: "14px",
+      }}
+    >
+      {name}
     </div>
-
+    <div
+      style={{
+        width: "100%",
+        marginBottom: "12px",
+      }}
+    />
+    {Object.keys(data).map((key, i) => {
+      return (
+        <div
+          key={i}
+          style={{
+            display: "flex",
+            marginBottom: "6px",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: data[key],
+              borderColor: data[key],
+              opacity: "1",
+              height: "16px",
+              width: "50px",
+              marginRight: "6px",
+            }}
+          />
+          <div style={{ fontSize: "14px" }}>{key}</div>
+        </div>
+      );
+    })}
+  </div>
+);
 
 export default Key;
