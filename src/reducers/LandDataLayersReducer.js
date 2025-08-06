767ea035029302c206eee8590f00bbd9d40d78e2
ms0ur1s <msouris@gmail.com>
2025-08-06 14:58:16 +0100

    LandDataLayersReducer reverted to dev version

--- src/reducers/LandDataLayersReducer.js
+++ src/reducers/LandDataLayersReducer.js
@@ -6,44 +6,16 @@ let layerId;
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
       case "TOGGLE_LAND_DATA_LAYER":
-        const toggleLayerId = action.payload;
-
-        /**
-         * #361 - Handle toggling of land data layers
-         * If the layer is already in the array, remove it
-         */
-        if (state.landDataLayers.includes(toggleLayerId)) {
-          return {
-            ...state,
-            landDataLayers: state.landDataLayers.filter(
-              (id) => id !== toggleLayerId
-            ),
-          };
+        landDataLayers = state.landDataLayers.slice();
+        layerId = action.payload;
+        if (landDataLayers.includes(layerId)) {
+          landDataLayers = landDataLayers.filter((e) => e !== layerId);
         } else {
-          return {
-            ...state,
-            landDataLayers: [...state.landDataLayers, toggleLayerId],
-          };
+          landDataLayers.push(layerId);
         }
-      case "ADD_LAYER_TO_KEY":
-        if (!state.landDataLayers.includes(action.payload)) {
-          return {
-            ...state,
-            landDataLayers: [...state.landDataLayers, action.payload],
-          };
-        }
-        return state;
-      case "REMOVE_LAYER_FROM_KEY":
         return {
           ...state,
-          landDataLayers: state.landDataLayers.filter(
-            (id) => id !== action.payload
-          ),
-        };
-      case "CLEAR_MAP_LAYERS":
-        return {
-          ...state,
-          landDataLayers: [],
+          landDataLayers: landDataLayers,
         };
       case "LOAD_MAP":
         return action.payload.data.mapLayers;
