22c7e3809f6b85c0bc6cd618e0b85691a6982aa7
Laura Thackray <laurasoftware21@gmail.com>
2026-05-12 16:41:35 +0100

    NewMap - remove canToggle prop

--- src/components/modals/NewMap.tsx
+++ src/components/modals/NewMap.tsx
@@ -8,36 +8,38 @@ const NewMap = () => {
     const { saving, saveError } = useAppSelector((state) => state.mapMeta);
     const mapSaved = !isNewMap && !saving && !saveError;
 
-    return <Modal id="newMap" canToggle={true} padding={true}>
+    return (
+      <Modal id="newMap" padding={true}>
         <div className="modal-title">New Map</div>
         <div className="modal-content modal-padding">
-            <div>
-                {mapSaved ?
-                    'Are you sure you wish to open a new map?' : 'Any unsaved changes to the current map will be lost.'
-                }
-            </div>
+          <div>
+            {mapSaved
+              ? "Are you sure you wish to open a new map?"
+              : "Any unsaved changes to the current map will be lost."}
+          </div>
         </div>
         <div className="modal-buttons">
-            <div className="button button-cancel rounded-button-full  modal-button-cancel"
-                onClick={() => {
-                    dispatch({ type: 'CLOSE_MODAL', payload: 'newMap' });
-                }}
-            >
-                Cancel
-            </div>
-            <div className="button rounded-button-full  modal-button-confirm"
-                onClick={async () => {
-                    dispatch({ type: 'CLOSE_MODAL', payload: 'newMap' });
-                    await dispatch(autoSave() as any);
-                    dispatch(newMap() as any);
-                }}
-            >
-                {mapSaved ?
-                    'Yes' : 'Ok'
-                }
-            </div>
+          <div
+            className="button button-cancel rounded-button-full  modal-button-cancel"
+            onClick={() => {
+              dispatch({ type: "CLOSE_MODAL", payload: "newMap" });
+            }}
+          >
+            Cancel
+          </div>
+          <div
+            className="button rounded-button-full  modal-button-confirm"
+            onClick={async () => {
+              dispatch({ type: "CLOSE_MODAL", payload: "newMap" });
+              await dispatch(autoSave() as any);
+              dispatch(newMap() as any);
+            }}
+          >
+            {mapSaved ? "Yes" : "Ok"}
+          </div>
         </div>
-    </Modal>;
+      </Modal>
+    );
 }
 
 export default NewMap;
