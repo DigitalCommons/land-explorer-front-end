3350778f2f0b0c7ab6c6311c0d592a6bacf94f1d
Laura Thackray <laurasoftware21@gmail.com>
2026-05-20 14:05:47 +0100

    update analytics

--- src/analytics.ts
+++ src/analytics.ts
@@ -17,17 +17,10 @@ export function initializeMixpanel(): void {
   });
 }
 
-let userId: string | null = null;
-let user: string | undefined;
-
 /** Set (anonymized) user in the Mixpanel event data */
-export const setAnalyticsUser = async (id: string, username: string) => {
+export const setAnalyticsUser = async (userId: string, username: string) => {
   console.log(`[ANALYTICS] setUser`);
-  if (userId !== id) {
-    // Only need to re-compute hash if the user ID has changed
-    userId = id;
-    user = await getUserHash(id, username);
-  }
+  const user = await getUserHash(userId, username);
   mixpanel.identify(user);
 };
 
@@ -35,15 +28,13 @@ export const setAnalyticsUser = async (id: string, username: string) => {
 export const resetAnalyticsUser = () => {
   console.log(`[ANALYTICS] resetUser`);
   mixpanel.reset();
-  userId = null;
-  user = "LOGGED_OUT";
 };
 
 /**
  * Convert a userId to a hashed value, using their username as a salt, to anonymize it for
  * analytics. This must match with the back-end's implementation, so analytics can be correlated.
  */
-const getUserHash = async (id: string, username: string) => {
+const getUserHash = async (userId: string, username: string) => {
   const saltAndPepperedInput = `${userId}${username}${constants.MIXPANEL_PEPPER}`;
 
   // Compute SHA-256 hash
