92d067d1f29349cba42ce4df9889a9d28bfd6fbc
Laura Thackray <laurasoftware21@gmail.com>
2026-05-27 14:40:33 +0100

    privacy settings page part 2

--- src/pages/PrivacySettings.tsx
+++ src/pages/PrivacySettings.tsx
@@ -1,9 +1,27 @@
+import { setAnalyticsConsent } from "@/actions/UserActions";
 import ToggleSwitch from "@/components/common/ToggleSwitch";
-import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "@/hooks/react-redux";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const PrivacySettings = () => {
-  const changePrivacySettings = (e: React.FormEvent) => {
-    e.preventDefault();
+  const dispatch = useAppDispatch();
+  const navigator = useNavigate();
+  const analyticsConsent = useAppSelector(
+    (state) => state.user.analyticsConsent,
+  );
+
+  const [currentAnalyticsConsent, setCurrentAnalyticsConsent] = useState(
+    analyticsConsent ?? false,
+  );
+
+  const toggleAnalyticsConsent = async () => {
+    setCurrentAnalyticsConsent(!currentAnalyticsConsent);
+  };
+
+  const savePrivacySettings = async () => {
+    await dispatch(setAnalyticsConsent(currentAnalyticsConsent));
+    navigator("/app/my-account");
   };
 
   return (
@@ -14,23 +32,26 @@ const PrivacySettings = () => {
         <p className="privacy-settings__copy">
           {" "}
           We use pseudonymous* analytics to understand how people use Land
-          Explorer and improve the service. You can turn this on/off at any time
-          using the toggle below.
+          Explorer and improve the service.
+          <br />
+          <br />
+          You can turn this on/off at any time using the toggle below.
         </p>
-
-        <form onSubmit={changePrivacySettings}>
-          <div className="privacy-settings__toggle-group">
-            <ToggleSwitch
-              on={true}
-              toggle={() => {}}
-              tooltip="Enable or disable analytics tracking"
-            />
-            <span className="privacy-settings__toggle-label">
-              Allow Analytics
-            </span>
-          </div>
-        </form>
-
+        <div className="privacy-settings__toggle-group">
+          <ToggleSwitch
+            on={currentAnalyticsConsent === true}
+            toggle={toggleAnalyticsConsent}
+            tooltip="Enable or disable analytics tracking"
+          />
+          <span className="privacy-settings__toggle-label">
+            Allow Analytics
+          </span>
+        </div>
+        {/* <div className="privacy-settings__button-group"> */}
+        <button className="rounded-button" onClick={savePrivacySettings}>
+          Save Changes
+        </button>
+        {/* </div> */}
         <p className="privacy-settings__copy-footer">
           *This means analytics data may be linked to an identifier, but not to
           your real name.
